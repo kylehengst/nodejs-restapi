9eeefcf6ae8e36d9a88bf5ce1ecd371a5dadcadf
Kyle Hengst <kylhengs@ausydrad0263.local>
2016-03-30 11:32:26 +1100

    updates

--- server.js
+++ server.js
@@ -2,10 +2,53 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
+var id = 2;
+
+app.get('/deleteUser', function (req, res) {
+
+    // First read existing users.
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+        data = JSON.parse( data );
+        delete data["user" + 2];
+
+        console.log( data );
+        res.end( JSON.stringify(data));
+    });
+})
+
+app.get('/addUser', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+        data = JSON.parse( data );
+        data["user4"] = user["user4"];
+        console.log( data );
+        res.end( JSON.stringify(data));
+    });
+})
+
 app.get('/listUsers', function (req, res) {
     fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
         console.log( data );
         res.end( data );
+    });
+})
+
+var user = {
+    "user4" : {
+        "name" : "mohit",
+        "password" : "password4",
+        "profession" : "teacher",
+        "id": 4
+    }
+}
+
+app.get('/:id', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+        users = JSON.parse( data );
+        var user = users["user" + req.params.id]
+        console.log( user );
+        res.end( JSON.stringify(user));
     });
 })
 
